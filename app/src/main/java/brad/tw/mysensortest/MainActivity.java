4846ebe60ae5e0ae32d8f3a1e86f84fda6a28080
Brad <brad@brad.tw>
2016-10-15 15:25:36 +0800

    v4

--- app/src/main/java/brad/tw/mysensortest/MainActivity.java
+++ app/src/main/java/brad/tw/mysensortest/MainActivity.java
@@ -1,6 +1,8 @@
 package brad.tw.mysensortest;
 
 import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
@@ -11,6 +13,8 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private SensorManager mSensorManager;
+    private Sensor sensor;
+    private MySensorListener listener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +34,39 @@ public class MainActivity extends AppCompatActivity {
             Log.v("brad", name + ":" + type + ":" + vendor);
         }
 
+        sensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        if (sensor == null){
 
+        }
+
+
+
+    }
+
+    private class MySensorListener implements SensorEventListener {
+
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        listener = new MySensorListener();
+        mSensorManager.registerListener(listener,sensor,
+                SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(listener);
     }
 }
